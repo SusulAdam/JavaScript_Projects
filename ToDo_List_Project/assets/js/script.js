01072b5a82a4cf33fe1bfe67efd32a4d99020d2b
Adam Susuł <a.w.susul@gmail.com>
2020-07-20 18:07:00 +0200

    createToolsArea and addNewTask fucntion added

--- ToDo_List_Project/assets/js/script.js
+++ ToDo_List_Project/assets/js/script.js
@@ -1,25 +1,24 @@
-let $todoInput; // input
-let $alertInfo; // info o braku zadan
-let $addBtn; // przcisk
-let $ulList; // lista zadan
-let $newTask; // nowy dodany li
-let $popup; // pobrany popup
-let $popupinfo; // alert w popupie, jak sie doda pusty tekst
-let $editedTodo; //edtytowany Todo
-let $popupInput; // tekst wpisywany w input
-let $addPopupBtn; // przycisk "zatwierdz" w popup'e
-let $closeTodoBtn; // przycisk do zamykania popup'a
+let $todoInput;
+let $alertInfo;
+let $addBtn;
+let $ulList;
+let $newTask;
+let $popup;
+let $popupinfo;
+let $editedTodo;
+let $popupInput;
+let $addPopupBtn;
+let $closeTodoBtn;
 let $idNumber = 0;
 let $allTask;
 
 
 const main = () => {
     prepareDOMElements();
-    prepareDOMEvens();
+    prepareDOMEvents();
 }
 
 
-// pobieramy elelemnty
 const prepareDOMElements = () => {
     $todoInput = document.querySelector('.todo__input');
     $alertInfo = document.querySelector('.todo__list--info');
@@ -32,4 +31,46 @@ const prepareDOMElements = () => {
     $closeTodoBtn = document.querySelector('.cancel');
     $allTask = $ulList.getElementsByTagName('li');
 }
+
+const prepareDOMEvents = () => {
+    $addBtn.addEventListener('click', addNewTask)
+}
+
+
+const addNewTask = () => {
+    if ($todoInput.value !== '') {
+        $newTask = document.createElement('li');
+        $newTask.innerText = $todoInput.value;
+        $ulList.appendChild($newTask);
+        $todoInput.value = ""
+        $alertInfo.innerText = ""
+        createToolsArea()
+    } else {
+        $alertInfo.innerText = "Enter the content of task!"
+    }
+}
+
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div');
+    toolsPanel.classList.add('todo__tool');
+    $newTask.appendChild(toolsPanel)
+
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('todo__complete');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+    toolsPanel.appendChild(completeBtn)
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('todo__edit');
+    editBtn.innerText = 'EDIT';
+    toolsPanel.appendChild(editBtn);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('todo__delte');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+    toolsPanel.appendChild(deleteBtn);
+
+}
+
+document.addEventListener('DOMContentLoaded', main)
 
