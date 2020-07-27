f84750748e164d6437029af746e8d2bcb9997717
Adam Susuł <a.w.susul@gmail.com>
2020-07-27 19:22:13 +0200

    dots are workings

--- Sliders_ App- HTML5_SCSS_JS/assets/js/script.js
+++ Sliders_ App- HTML5_SCSS_JS/assets/js/script.js
@@ -1,11 +1,23 @@
 let slideIndex = 0;
 
-const showSlides = () => {
+const first_btn = document.querySelector("#first-btn")
+first_btn.addEventListener('click', function () {
+    slideIndex = 0;
+})
 
+const second_btn = document.querySelector("#second-btn")
+second_btn.addEventListener('click', function () {
+    slideIndex = 1;
+})
+
+const third_btn = document.querySelector("#third_btn")
+third_btn.addEventListener('click', function () {
+    slideIndex = 2;
+})
+
+const showSlides = () => {
     const slides = document.getElementsByClassName("slider__mySlides");
     const dots = document.getElementsByClassName("slider__dot");
-
-
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"
 
@@ -23,8 +35,10 @@ const showSlides = () => {
     slides[slideIndex - 1].style.display = "block"
     dots[slideIndex - 1].className += " active"
 
-    setTimeout(showSlides, 3000);
+
+    x = console.log(setTimeout(showSlides, 3000))
 
 };
 
 showSlides();
+
