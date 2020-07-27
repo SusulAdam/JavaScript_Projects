63a65c68ff47ba02b028ae7041afb41fb549a938
Adam Susuł <a.w.susul@gmail.com>
2020-07-27 14:41:13 +0200

    Demo version of Slider

--- Sliders_ App- HTML5_SCSS_JS/assets/js/script.js
+++ Sliders_ App- HTML5_SCSS_JS/assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("test")
+let slideIndex = 0;
+
+const showSlides = () => {
+
+    const slides = document.getElementsByClassName("slider__mySlides");
+    const dots = document.getElementsByClassName("slider__dot");
+
+    console.log(slides);
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+
+    slides[slideIndex - 1].style.display = "block"
+    dots[slideIndex - 1].className += " active"
+
+    setTimeout(showSlides, 3000);
+
+};
+
+showSlides();
