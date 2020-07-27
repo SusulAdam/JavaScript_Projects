2902d32ca074eb8f2857d9c10548102c2b248491
Adam Susuł <a.w.susul@gmail.com>
2020-07-27 21:51:39 +0200

    Created new project- Swiper_JS-HTML5_CSS3_SWIPPER-JS

---
+++ Swiper_JS-HTML5_CSS3_SWIPER-JS/assets/js/main.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    parallax: true,
+    speed: 600,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
