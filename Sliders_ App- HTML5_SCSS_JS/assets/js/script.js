39588bfc48c8aab42e5c8998204f0a1e09950a57
Adam Susuł <a.w.susul@gmail.com>
2020-07-27 15:24:42 +0200

    webkit animation added

--- Sliders_ App- HTML5_SCSS_JS/assets/js/script.js
+++ Sliders_ App- HTML5_SCSS_JS/assets/js/script.js
@@ -5,14 +5,13 @@ const showSlides = () => {
     const slides = document.getElementsByClassName("slider__mySlides");
     const dots = document.getElementsByClassName("slider__dot");
 
-    console.log(slides);
 
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"
+
     }
 
     slideIndex++;
-
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
